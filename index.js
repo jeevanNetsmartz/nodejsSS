dfe52b6f1f575f133ec3d7d16116a58cfc40440d
Jeevan Singh <jeevan.s@netsmartz.net>
2022-05-03 18:42:37 +0530

    nodejsss

---
+++ index.js
@@ -0,0 +1,36 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/view', (req, res) => {
+    res.sendFile(__dirname + '/display.html');
+})
+
+io.on('connection', (socket)=> {
+    console.log("socket connected to http://localhost:8080");
+    socket.on("join-message", (roomId) => {
+        socket.join(roomId);
+        console.log("User joined in a room : " + roomId);
+    })
+
+    socket.on("screen-data", function(data) {
+        data = JSON.parse(data);
+        var room = data.room;
+        var imgStr = data.image;
+        socket.broadcast.to(room).emit('screen-data', imgStr);
+    })
+
+    socket.on("canvas", function(data) {
+        console.log(data)
+        // data = JSON.parse(data);
+        // var room = data.room;
+        // var imgStr = data.image;
+        io.sockets.emit("canvas", 'nhnnnnnnnnnnnnnnnnnnn')
+        // socket.broadcast.emit("canvas", 'nhnnnnnnnnnnnnnnnnnnn');
+    })
+})
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+http.listen(server_port, () => {
+    console.log("Started on : "+ server_port);
+})
